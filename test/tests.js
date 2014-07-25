406f97539ad1b1d255a202e4722b35efcaebc3e8
Chuan Miao <chuan.miao@kit.edu>
2014-07-25 21:18:56 +0200

    Initial commit

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+// ractive-decorators-ractive-table-decorator tests
+// ==========================================
+
+(function () {
+
+	var fixture;
+
+	test( 'We are ready to write some tests', function ( t ) {
+		t.ok( true );
+	});
+
+
+}());
